529bfb542a4f1d5f6436bde5f9657ca18554fb22
ranhershko <ran.hershko@gmail.com>
2019-12-28 11:48:46 +0200

    Update age_to_bucket_parser.py

--- json_2_yaml/age_to_bucket_parser.py
+++ json_2_yaml/age_to_bucket_parser.py
@@ -23,13 +23,13 @@ def yaml_my_json_group_sorted():
     people_list = sorted(data["ppl_ages"].items(), key=lambda kv: kv[AGE])
     extra_age = [0, people_list[LAST_INDEX][AGE] + 1]
     ages_borders.extend(extra_age)
-    sorted_ages_broders = sorted(ages_borders)
+    sorted_ages_borders = sorted(ages_borders)
 
     current_people_list = people_list
-    for age_border in range(len(sorted_ages_broders) - 1):
-        print(sorted_ages_broders[age_border], "-", sorted_ages_broders[age_border + NEXT_AGE_BORDER], ":", sep="")
+    for age_border in range(len(sorted_ages_borders) - 1):
+        print(sorted_ages_borders[age_border], "-", sorted_ages_borders[age_border + NEXT_AGE_BORDER], ":", sep="")
         for person in current_people_list:
-            if sorted_ages_broders[age_border] <= person[AGE] < sorted_ages_broders[age_border + NEXT_AGE_BORDER]:
+            if sorted_ages_borders[age_border] <= person[AGE] < sorted_ages_borders[age_border + NEXT_AGE_BORDER]:
                 print("  -", person[NAME])
             else:
                 current_people_list = people_list[people_list.index(person):]
