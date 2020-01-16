e5aa30be371de1842f256639340a178e7bc3203f
ran.hershko <ran.hershko@gmail.com>
2020-01-16 17:13:31 +0200

    Update weather.py

--- weather/weather.py
+++ weather/weather.py
@@ -4,7 +4,7 @@ from requests import get
 
 @click.command()
 @click.option(
-  '--temperature', '-T',
+  '--temperature_unit', '-T',
   type=click.Choice(['Celsius', 'Fahrenheit'], case_sensitive=False),
   default='Celsius',
   help=("Weather temperature units type. "
@@ -15,18 +15,17 @@ from requests import get
 @click.option("--city", '-c', help="Cities names separated by ',' or ';'. "
               "A city name that contains more than an one word must be enclosed in brackets(\")", required=True)
 @click.option("--token", '-t', help="The weatherstack.com user API Token ", required=True)
-def cli(temperature, city, token):
-    cities = city.replace(';', ',').split(',')
-    for_supplied_cities = [city_exists for city_exists in cities if city_exists]
-    in_temperature_unit = temperature
+def cli(temperature_unit, city, token):
+#     cities = city.replace(';', ',').split(',')
+    for_supplied_cities = [not_empty_city_name for not_empty_city_name in city.replace(';', ',').split(',') if not_empty_city_name]
     using_api_token = token
-    print_city_weather(for_supplied_cities, in_temperature_unit, using_api_token)
+    print_city_weather(for_supplied_cities, temperature_unit, using_api_token)
 
 
-def print_city_weather(cities, temperature, token):
-    temperature_unit = "m" if temperature == 'Celsius' else "f"
+def print_city_weather(cities, temperature_unit, token):
+    temperature_unit_Acronyms = "m" if temperature_unit == 'Celsius' else "f"
     for city in cities:
-        url = f"http://api.weatherstack.com/current?access_key={token}&query={city}&units={temperature_unit}"
+        url = f"http://api.weatherstack.com/current?access_key={token}&query={city}&units={temperature_unit_Acronyms}"
         json_out = get(url).json()
         if 'request' not in json_out:
             if json_out["error"]["type"] == "invalid_access_key":
@@ -42,7 +41,7 @@ def print_city_weather(cities, temperature, token):
                 print(msg)
                 exit(615)
         print(f"The weather in {json_out['request']['query']} today {json_out["current"]["temperature"]} \
-{temperature}")
+{temperature_unit}")
 
 
 if __name__ == '__main__':
