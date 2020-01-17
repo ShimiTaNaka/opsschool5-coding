f1b9054ecc3382414598fe75f0b74e174f95fc14
toly <toly.sukhotin@gmail.com>
2020-01-17 11:28:13 +0200

    fixes

--- hw2/weather-api.py
+++ hw2/weather-api.py
@@ -1,23 +1,14 @@
 from requests import get
 import click
-
+import sys
 BASE_URL = "http://api.weatherstack.com/current"
 
 
-def get_temperature_unit(temperature_name):
-    names_units = {"Celsius": "m", "Fahrenheit": "f"}
-    temperature_unit = None
-    try:
-        temperature_unit = names_units[temperature_name]
-    except KeyError:
-        print("The temperature name '{}' doesn't exist.".format(temperature_name))
-        keys_list = list(names_units.keys())
-        print("Please choose {0} or {1}.".format(keys_list[0], keys_list[1]))
-    return temperature_unit
+def print_error(code, message):
+    print("Error code: {0}. Message: {1} ".format(code, message))
 
 
 def get_temperature(token, city_name, units):
-    temperature = None
     good_response_code = 200
     request_failed_code = 615
     params = {'access_key': token, 'units': units, 'query': city_name}
@@ -30,26 +21,30 @@ def get_temperature(token, city_name, units):
             temperature = None
             error_code = response_json["error"]["code"]
             if error_code == request_failed_code:
-                print("The city '{}; doesn't exist. Please try another one. For example Dublin".format(city_name))
+                print_error(error_code, "The city '{}; doesn't exist. "
+                                        "Please try another one. For example Dublin.".format(city_name))
             else:
-                print(response_json["error"]["info"])
+                print_error(error_code, response_json["error"]["info"])
+                sys.exit(1)
     else:
-        print(response.content)
+        print_error(response.status_code, response.content)
+        sys.exit(1)
     return temperature
 
 
 @click.command()
 @click.option('--token', required=True, help="API Token for weatherstack.com")
 @click.option('--city', required=True, help="One or more comma separated cities.For example \"New York\",Dublin")
-@click.option('--temperature_name', '--T', default='Celsius', help="Weather unit.'Celsius'(Default) or 'Fahrenheit'")
+@click.option('--temperature_name', '--T', default='Celsius',
+              type=click.Choice(['Celsius', 'Fahrenheit'], case_sensitive=False))
 def main(token, city, temperature_name):
+    names_units = {"celsius": "m", "fahrenheit": "f"}
     cities = city.split(",")
-    temperature_unit = get_temperature_unit(temperature_name)
-    if temperature_unit:
-        for city_name in cities:
-            temperature = get_temperature(token, city_name, temperature_unit)
-            if temperature:
-                print("The weather in {0} today {1} {2}".format(city_name, temperature, temperature_name))
+    temperature_unit = names_units[temperature_name.lower()]
+    for city_name in cities:
+        temperature = get_temperature(token, city_name, temperature_unit)
+        if temperature is not None:
+            print("The weather in {0} today {1} {2}".format(city_name, temperature, temperature_name.capitalize()))
 
 
 if __name__ == '__main__':
